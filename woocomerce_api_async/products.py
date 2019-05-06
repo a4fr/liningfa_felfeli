a244705a95f2a9801d498c57ac0774e546033ea0
Ali Najafi <mail.ali.najafi@gmail.com>
2019-05-06 18:06:19 +0430

    support semaphore

--- woocomerce_api_async/products.py
+++ woocomerce_api_async/products.py
@@ -8,26 +8,34 @@ from pprint import pprint
 import asyncio
 
 
-async def add_variation(wcapi: WC_API, pid, attribute, regular_price, sale_price):
+async def add_variation(wcapi: WC_API, pid, attribute, regular_price, sale_price, asyc_semaphore):
     data = {
         "regular_price": regular_price,
         "sale_price": sale_price,
         "attributes": list(),
     }
     data["attributes"].append(attribute)
-    return await wcapi.post("products/%s/variations" % pid, data)
+    async with asyc_semaphore:
+        return await wcapi.post("products/%s/variations" % pid, data)
+    return resp
 
 
-async def get_all_variations(wcapi: WC_API, pid):
-    return await wcapi.get("products/%s/variations" % pid)
+async def get_all_variations(wcapi: WC_API, pid, asyc_semaphore):
+    async with asyc_semaphore:
+        resp = await wcapi.get("products/%s/variations" % pid)
+    return resp
 
 
-async def delete_variation(wcapi: WC_API, pid, vid):
-    return await wcapi.delete("products/%s/variations/%s" % (pid, vid))
+async def delete_variation(wcapi: WC_API, pid, vid, asyc_semaphore):
+    async with asyc_semaphore:
+        resp = await wcapi.delete("products/%s/variations/%s" % (pid, vid))
+    return resp
 
 
-async def delete_product(wcapi: WC_API, pid, forced=True):
-    return await wcapi.delete("products/%s?force=%s" % (pid, str(forced).lower()))
+async def delete_product(wcapi: WC_API, pid, asyc_semaphore, forced=True):
+    async with asyc_semaphore:
+        resp = await wcapi.delete("products/%s?force=%s" % (pid, str(forced).lower()))
+    return resp
 
 
 if __name__ == '__main__':
